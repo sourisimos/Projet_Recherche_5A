b38d43ba48da1b7a58b7c3f6211664fd01cdfdaf
Sourisimos <sourisimos@gmail.com>
2024-11-11 18:43:39 +0100

    minor changes

--- Code/maillage.py
+++ Code/maillage.py
@@ -25,8 +25,9 @@ class MaillageDelaunayMultiDimension:
 
         point : np.array de taille (input_dim,) - coordonnées du point à évaluer.
 
-        Retourne : un tableau numpy de dimension (output_dim) contenant la valeur
+        Retourne : - un tableau numpy de dimension (output_dim) contenant la valeur
                    interpolée de la fonction en ce point, ou None si en dehors du maillage.
+                   - rien si le point est hors du maillage 
         """
         simplex = self.regions.find_simplex(point)
 
@@ -56,26 +57,28 @@ class MaillageDelaunayMultiDimension:
         if self.input_dim >= 2 or self.output_dim != 1:
             print("La visualisation 3D est uniquement possible pour un maillage en entrée 2D "
                   "et des valeurs de sortie 3D !")
-            return
+            
+        else : 
+            fig = plt.figure(figsize=(10, 8))
+            ax = fig.add_subplot(111, projection="3d")
 
-        fig = plt.figure(figsize=(10, 8))
-        ax = fig.add_subplot(111, projection="3d")
+            # Création d'une collection de triangles pour la surface 3D en utilisant la 3e dimension des valeurs
+            for simplex in self.regions.simplices:
+                vertices = self.points[simplex]
+                # Mapping chaque sommet à sa coordonnée (x, y, z) interpolée
+                reg_vertices = [(vertices[i][0], vertices[i][1], self.values_at_vertices[simplex[i], 0]) for i in range(3)]
+                color_value = np.mean(self.values_at_vertices[simplex, 0])
+                poly = Poly3DCollection([reg_vertices], color=plt.cm.viridis(color_value), edgecolor="k", alpha=0.7)
+                ax.add_collection3d(poly)
 
-        # Création d'une collection de triangles pour la surface 3D en utilisant la 3e dimension des valeurs
-        for simplex in self.regions.simplices:
-            vertices = self.points[simplex]
-            # Mapping chaque sommet à sa coordonnée (x, y, z) interpolée
-            reg_vertices = [(vertices[i][0], vertices[i][1], self.values_at_vertices[simplex[i], 0]) for i in range(3)]
-            color_value = np.mean(self.values_at_vertices[simplex, 0])
-            poly = Poly3DCollection([reg_vertices], color=plt.cm.viridis(color_value), edgecolor="k", alpha=0.7)
-            ax.add_collection3d(poly)
+            # Paramètres des axes
+            ax.set_xlabel("X")
+            ax.set_ylabel("Y")
+            ax.set_zlabel("Valeur de la fonction (3ème dimension de sortie)")
+            ax.set_title("Visualisation 3D de la fonction interpolée sur le maillage")
 
-        # Paramètres des axes
-        ax.set_xlabel("X")
-        ax.set_ylabel("Y")
-        ax.set_zlabel("Valeur de la fonction (3ème dimension de sortie)")
-        ax.set_title("Visualisation 3D de la fonction interpolée sur le maillage")
+            # Échelle de couleurs
+            plt.colorbar(plt.cm.ScalarMappable(cmap="viridis"), ax=ax, label="Valeur de la fonction", shrink=0.5, pad=0.1)
+            plt.show()
 
-        # Échelle de couleurs
-        plt.colorbar(plt.cm.ScalarMappable(cmap="viridis"), ax=ax, label="Valeur de la fonction", shrink=0.5, pad=0.1)
-        plt.show()
+        return None
